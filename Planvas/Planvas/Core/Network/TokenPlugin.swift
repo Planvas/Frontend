49c1ab2d9d91ca04ba7e8963e6b5d12ff85d1632
minkyo <smk78449@naver.com>
2026-01-17 19:41:25 +0900

    ✨ Feat: 토큰 만료 처리용 Moya 플러그인 구현 (#4)

--- Planvas/Planvas/Core/Network/TokenPlugin.swift
+++ Planvas/Planvas/Core/Network/TokenPlugin.swift
@@ -1,21 +1,15 @@
-//
-//  AuthPlugin.swift
-//  palnBas_mango
-//
-//  Created by 송민교 on 1/16/26.
-//
 import Foundation
 import Moya
 
-struct AuthPlugin: PluginType {
+struct TokenPlugin: PluginType {
+    /// 네트워크 응답을 받은 후 토큰 만료(401)여부 체크
     func didReceive(_ result: Result<Response, MoyaError>, target: any TargetType) {
         switch result {
         case .success(let response):
             if response.statusCode == 401 {
                 print("AccessToken 만료")
-                
                 KeychainManager.shared.delete(key: "accessToken")
-                
+                // TODO: - 토큰 만료 시 로그인화면으로 이동 or 토큰 갱신 로직이 필요
             }
         case .failure(let error):
             print(error.localizedDescription)
