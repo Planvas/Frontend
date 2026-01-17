03d398ca868dbe89f6c912a62f5070a4a0ab9103
minkyo <smk78449@naver.com>
2026-01-17 19:42:08 +0900

    ✨ Feat: TokenProvider 구현

--- Planvas/Planvas/Service/Common/TokenProvider.swift
+++ Planvas/Planvas/Service/Common/TokenProvider.swift
@@ -1,16 +1,11 @@
-//
-//  TokenProvider.swift
-//  palnBas_mango
-//
-//  Created by 송민교 on 1/16/26.
-//
 import Foundation
 import Moya
 
-// 어떤 Router(Target)든 다 받아줌
+/// 어떤 Router(Target)든 다 받아주는 Provider
 final class TokenProvider<Target: APITargetType> {
-    private let provider = MoyaProvider<Target>(plugins: [AuthPlugin()])
+    private let provider = MoyaProvider<Target>(plugins: [TokenPlugin()])
     
+    /// 네트워크 요청 실행, 결과를 Decodable 타입으로 반환
     func request<T: Decodable>(_ target: Target, completion: @escaping (Result<T, Error>) -> Void) {
         provider.request(target) { result in
             switch result {
