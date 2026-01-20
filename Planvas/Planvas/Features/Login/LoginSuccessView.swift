210709c0826dae3ae2aedfc031349b2119b69827
minkyo <smk78449@naver.com>
2026-01-21 02:59:34 +0900

    ✨ Feat: 로그인 View 구현

---
+++ Planvas/Planvas/Features/Login/LoginSuccessView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct LoginSuccessView: View {
+    @EnvironmentObject var viewModel: LoginViewModel
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.purple1.opacity(0.2), Color.white]),
+                startPoint: .bottom,
+                endPoint: .top)
+            .ignoresSafeArea()
+            
+            VStack {
+                Image(.login)
+                    .resizable()
+                    .frame(width: 174, height: 213)
+                    .padding(.vertical, 50)
+                
+                VStack {
+                    Text("로그인 완료!")
+                        .foregroundStyle(Color.gray2)
+                        .textStyle(.medium20)
+                        .padding()
+                    Group {
+                        Text("\(viewModel.userName)님,")
+                        Text("환영해요!")
+                    }
+                    .textStyle(.bold30)
+                }
+            }
+        }
+    }
+}
