07d9bcc8c5aac536f3a00d2012ee743d9771bf4b
minkyo <smk78449@naver.com>
2026-01-21 02:59:34 +0900

    ✨ Feat: 로그인 View 구현

--- Planvas/Planvas/Features/Login/LoginView.swift
+++ Planvas/Planvas/Features/Login/LoginView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct LoginView: View {
+    @StateObject private var viewModel = LoginViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.background)
+                .resizable()
+                .frame(width: 404, height: 404)
+                .offset(y: -80)
+            
+            VStack {
+                Spacer()
+                    .frame(height: 100)
+                
+                Image(.start)
+                    .resizable()
+                    .frame(width: 185, height: 185)
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                VStack {
+                    Text("Planvas")
+                        .textStyle(.extrabold45)
+                        .linearGradient(startColor: .grad1, endColor: .purple1)
+                        .padding(.vertical, 5)
+                    Group {
+                        Text("내가 그리는 모습 그대로,")
+                        Text("일상을 채워나가는 밸런스 플래너")
+                    }
+                    .textStyle(.semibold20)
+                }
+                
+                Spacer()
+                    .frame(height: 70)
+                
+                PlanvasButton(
+                    title: "Google로 시작하기",
+                    isDisabled: false,
+                    action: {
+                        viewModel.GoogleLogin()
+                    })
+                .padding()
+                .fullScreenCover(isPresented: $viewModel.isLoginSuccess){
+                    LoginSuccessView()
+                        .environmentObject(viewModel)
+                }
+            }
+        }
     }
 }
 
