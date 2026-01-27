4d0d0a6b3b2dbb2a3e212df23b057947d15350dd
minkyo <smk78449@naver.com>
2026-01-28 02:30:32 +0900

    ✨ Feat: Combine 추가(#17)

--- Planvas/Planvas/Service/Common/TokenProvider.swift
+++ Planvas/Planvas/Service/Common/TokenProvider.swift
@@ -1,9 +1,18 @@
 import Foundation
+import Combine
 import Moya
 
 /// 어떤 Router(Target)든 다 받아주는 Provider
 final class TokenProvider<Target: APITargetType> {
-    private let provider = MoyaProvider<Target>(plugins: [TokenPlugin()])
+    private let provider: MoyaProvider<Target>
+    
+    init(isStub: Bool = false) {
+        if isStub {
+            self.provider = MoyaProvider<Target>(stubClosure: MoyaProvider.immediatelyStub)
+        } else {
+            self.provider = MoyaProvider<Target>(plugins: [TokenPlugin()])
+        }
+    }
     
     /// 네트워크 요청 실행, 결과를 Decodable 타입으로 반환
     func request<T: Decodable>(_ target: Target, completion: @escaping (Result<T, Error>) -> Void) {
@@ -26,5 +35,20 @@ final class TokenProvider<Target: APITargetType> {
                 completion(.failure(error))
             }
         }
+    }
+    
+    /// Combine을 위한 requestPublisher
+    func requestPublisher<T: Decodable>(_ target: Target) -> AnyPublisher<T, Error> {
+        return Future<T, Error> { promise in
+            self.request(target) { (result: Result<T, Error>) in
+                switch result {
+                case .success(let decodedData):
+                    promise(.success(decodedData))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
