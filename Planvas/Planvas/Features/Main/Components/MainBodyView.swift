7332aaef57569dfbe51f2e0172ba9d0a6708814a
mzxxzysy <jsyjj21@gmail.com>
2026-01-28 13:58:40 +0900

    ♻️ Refactor: 캘린더 컴포넌트 분리 (#19)

--- Planvas/Planvas/Features/Main/Components/MainBodyView.swift
+++ Planvas/Planvas/Features/Main/Components/MainBodyView.swift
@@ -12,44 +12,8 @@ struct MainBodyView: View {
     @ObservedObject var viewModel: MainViewModel
     
     var body: some View {
-        VStack(alignment: .leading){
-            // MARK: - 위클리 캘린더
-            Text("이번 주")
-                .textStyle(.semibold25)
-                .padding(.top, 29)
-                .padding(.leading, 29)
-            
-            VStack(alignment: .leading){
-                Text(viewModel.monthText)
-                    .textStyle(.semibold20)
-                    .foregroundStyle(.black1)
-                    .padding(.bottom)
-                
-                HStack(spacing: 16) {
-                    ForEach(viewModel.weekDates, id: \.self) { date in
-                        DateItem(
-                            date: date,
-                            isSelected: viewModel.selectedDate == date
-                        )
-                        .onTapGesture {
-                            viewModel.selectedDate = date
-                        }
-                    }
-                }
-            }
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 15)
-                    .stroke(
-                        LinearGradient(
-                            colors: [.subPurple, .primary1],
-                            startPoint: UnitPoint(x: -0.1, y: -0.1),
-                            endPoint: UnitPoint(x: 0.2, y: 0.7)
-                        ),
-                        lineWidth: 1
-                    )
-            )
-            .frame(maxWidth: .infinity)
+        VStack {
+            CalendarGroup(monthText: viewModel.monthText, weekDates: viewModel.weekDates, selectedDate: $viewModel.selectedDate)
         }
     }
 }
