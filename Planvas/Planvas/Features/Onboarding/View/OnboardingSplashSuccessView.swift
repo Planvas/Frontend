4dc490d35f91b61df484493602376a8b1c39c926
hmj6589 <hmj6589@gmail.com>
2026-01-30 23:03:30 +0900

    ✨ Feat: 온보딩 스플래시 두번째 뷰 구현

---
+++ Planvas/Planvas/Features/Onboarding/View/OnboardingSplashSuccessView.swift
@@ -0,0 +1,44 @@
+//
+//  OnboardingSplashSuccessView.swift
+//  Planvas
+//
+//  Created by 황민지 on 1/30/26.
+//
+
+import SwiftUI
+
+struct OnboardingSplashSuccessView: View {
+    var body: some View {
+        ZStack {
+            Color.fff.ignoresSafeArea()
+
+            RadialGradient(
+                gradient: Gradient(stops: [
+                    .init(color: .goal178CC9.opacity(0.85), location: 0.00),
+                    .init(color: .primary1.opacity(0.63), location: 0.35),
+                    .init(color: .goal883AE1.opacity(0.52), location: 0.5),
+                    
+                    .init(color: .fff.opacity(0.0), location: 1.00)
+                ]),
+                center: .bottomLeading,
+                startRadius: 0,
+                endRadius: 750
+            )
+            .ignoresSafeArea()
+
+            
+            VStack(spacing: 0) {
+                Text("목표기간을")
+                
+                Text("설정해주세요")
+            }
+            .textStyle(.bold35)
+            .foregroundStyle(.black1)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        }
+    }
+}
+
+#Preview {
+    OnboardingSplashSuccessView()
+}
