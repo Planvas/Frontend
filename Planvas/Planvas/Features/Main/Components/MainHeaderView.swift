ef7ef2b8c6010bb90eb9cb3d1ea2c5d898e76352
mzxxzysy <jsyjj21@gmail.com>
2026-02-04 04:06:08 +0900

    💄 Design: 목표 있을 때 메인 페이지 퍼블리싱 (#19)

--- Planvas/Planvas/Features/Main/Components/MainHeaderView.swift
+++ Planvas/Planvas/Features/Main/Components/MainHeaderView.swift
@@ -11,46 +11,119 @@ import SwiftUI
 struct MainHeaderView: View {
     let goalSetting: GoalSetting
     let stateTitle: String
-    let stateDescription: String
-
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .modifier(
-                    GradientModifier(
-                        startColor: .subPurple,
-                        endColor: .primary1
-                    )
-                )
-
+        VStack {
             VStack(alignment: .leading) {
                 Image(.logo)
                     .resizable()
-                    .frame(width: 180, height: 43)
+                    .frame(width: 98, height: 23)
                     .padding(.top, 60)
-
+                
                 Text(stateTitle)
                     .lineLimit(nil)
                     .textStyle(.semibold30)
-                    .foregroundStyle(
-                        goalSetting == .ing ? .black1 : .fff
-                    )
-
-                Text(stateDescription)
-                    .textStyle(
-                        goalSetting == .ing ? .semibold16 : .semibold20
-                    )
-                    .foregroundStyle(.black1)
-                    .padding(.top, 10)
-
-                if goalSetting == .ing {
-                    PresentGoalGroup()
-                } else {
-                    HeaderButtonGroup(goalSetting: goalSetting)
+                    .foregroundStyle( .black1)
+                    .padding(.top, 36)
+                
+                HStack{
+                    Group{
+                        Text("D-17")
+                            .textStyle(.medium14)
+                            .foregroundStyle(.fff)
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 5)
+                            .background(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .foregroundStyle(.primary1)
+                            )
+                        
+                        Text("시험 기간 전 갓생")
+                            .textStyle(.medium14)
+                            .foregroundStyle(.fff)
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 5)
+                            .background(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .foregroundStyle(.primary50)
+                            )
+                    }
+                    .offset(y: -60)
+                    Spacer()
+                    Image(.mainCharacter)
+                        .offset(y: -40)
                 }
             }
             .padding()
-            .padding(.bottom, 20)
+            .background(.subPurple)
+            
+            ZStack{
+                VStack(spacing: 0){
+                    WaveBackground()
+                        .fill(.primary1)
+                    Rectangle()
+                        .fill(Color.primary1)
+                }
+                .frame(height: 420)
+                
+                VStack(alignment: .trailing) {
+                    if goalSetting == .ing {
+                        HStack{
+                            MainProgressView(
+                                type: "성장",
+                                goal: 60,
+                                progress: 40
+                            )
+                            Spacer()
+                            MainProgressView(
+                                type: "휴식",
+                                goal: 40,
+                                progress: 10
+                            )
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.bottom, 5)
+                        
+                        Text("바라는 모습대로 만든 균형에 맞춰\n일상을 채워보세요.\n그 시도만으로도")
+                            .textStyle(.medium18)
+                            .foregroundStyle(.subPurple)
+                            .multilineTextAlignment(.trailing)
+                        HStack(spacing: 0){
+                            Text("확실한 성취")
+                                .textStyle(.bold18)
+                                .foregroundStyle(.primary1)
+                                .background(.subPurple)
+                            Text(" 입니다.")                            .textStyle(.medium18)
+                                .foregroundStyle(.subPurple)
+                        }
+                    } else {
+                        HeaderButtonGroup(goalSetting: goalSetting)
+                    }
+                }
+                .padding(20)
+                .padding(.top, 67)
+            }
+            .offset(y: -180)
         }
     }
+}
+
+struct WaveBackground: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: rect.height * 0.35))
+            path.addCurve(
+                to: CGPoint(x: rect.width, y: rect.height * 0.4),
+                control1: CGPoint(x: rect.width * 0.4, y: rect.height * 0.1),
+                control2: CGPoint(x: rect.width * 0.7, y: rect.height * 0.55)
+            )
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.closeSubpath()
+        }
+    }
+}
+
+#Preview {
+    MainView()
 }
--- Planvas/Planvas/Features/Main/ViewModel/MainViewModel.swift
+++ Planvas/Planvas/Features/Main/ViewModel/MainViewModel.swift
@@ -16,22 +16,11 @@ class MainViewModel: ObservableObject {
     var StateTitle: String {
         switch goalSetting {
         case .ing:
-            return "지수님, 반가워요!"
+            return "지수님, \n반가워요!"
         case .end:
-            return "활동 기간이 \n종료되었어요"
+            return "지수님, \n활동 기간이 \n종료되었어요"
         case .none:
-            return "새로운 목표를 \n세워주세요!"
-        }
-    }
-    
-    var StateDescription: String {
-        switch goalSetting {
-        case .ing:
-            return "바라는 모습대로 만든 균형에 맞춰 일상을 채워보세요\n그 시도만으로도 확실한 성취입니다"
-        case .end:
-            return "목표한 균형을 잘 지켰는지 확인해보세요"
-        case .none:
-            return "이번 시즌,\n지수님이 바라는 모습은 무엇인가요?"
+            return "지수님, \n새로운 목표를 \n세워주세요!"
         }
     }
     
