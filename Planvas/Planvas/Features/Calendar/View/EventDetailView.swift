e6566d89fcd789a7480947d934c593ecd051dcdb
mzxxzysy <jsyjj21@gmail.com>
2026-02-05 01:01:05 +0900

    💄 Design: 메인 페이지 캘린더 퍼블리싱 (#19)

--- Planvas/Planvas/Features/Calendar/View/EventDetailView.swift
+++ Planvas/Planvas/Features/Calendar/View/EventDetailView.swift
@@ -310,20 +310,6 @@ extension View {
     }
 }
 
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(
-            roundedRect: rect,
-            byRoundingCorners: corners,
-            cornerRadii: CGSize(width: radius, height: radius)
-        )
-        return Path(path.cgPath)
-    }
-}
-
 #Preview {
     EventDetailView(
         event: Event(
--- Planvas/Planvas/Features/Main/Components/DateItem.swift
+++ Planvas/Planvas/Features/Main/Components/DateItem.swift
@@ -11,20 +11,36 @@ import SwiftUI
 struct DateItem: View {
     let date: Date
     let isSelected: Bool
+    @Binding var selectedDate: Date
+    let schedules: [Schedule]
     
     var body: some View {
         VStack(spacing: 6) {
             // 요일
             Text(date.weekday)
-                .font(.caption)
+                .textStyle(.medium14)
                 .foregroundColor(weekdayColor)
             
-            // 날짜
-            Text("\(date.day)")
-                .font(.headline)
-                .foregroundColor(dateTextColor)
-                .frame(width: 32, height: 32)
-                .background(backgroundView)
+            VStack{
+                // 날짜
+                Text("\(date.day)")
+                    .textStyle(.medium16)
+                    .foregroundColor(dateTextColor)
+                
+                // 일정 목록
+                VStack(spacing: 4) {
+                    ForEach(schedules) { schedule in
+                        scheduleView(schedule)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.vertical, 5)
+            .frame(width: 50, height: 93)
+            .background(backgroundView)
+        }
+        .onTapGesture {
+            selectedDate = date
         }
     }
     
@@ -35,6 +51,7 @@ struct DateItem: View {
         if date.isSaturday { return .blue1 }
         return .gray888
     }
+    
     // 날짜 주말, 선택된 날짜 색상
     private var dateTextColor: Color {
         if isSelected { return .gray888 }
@@ -42,14 +59,82 @@ struct DateItem: View {
         if date.isSaturday { return .blue1 }
         return .black1
     }
+    
     // 선택된 날짜 배경색
     private var backgroundView: some View {
         Group {
             if isSelected {
-                Color.ccc20
+                RoundedRectangle(cornerRadius: 7.5)
+                    .foregroundStyle(.ccc60)
             } else {
                 Color.clear
             }
         }
     }
+    
+    private func scheduleView(_ schedule: Schedule) -> some View {
+        let position = schedule.position(on: date)
+        let showTitle = schedule.shouldShowTitle(on: date)
+        
+        return HStack(spacing: 4) {
+            if position != .middle {
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(schedule.type.color)
+                    .frame(width: 3)
+            }
+
+            if showTitle {
+                Text(schedule.title)
+                    .textStyle(.medium10)
+                    .foregroundColor(.black1)
+                    .lineLimit(1)
+                    .clipped()
+            }
+
+            Spacer()
+        }
+        .padding(.vertical, 2)
+        .background(
+            scheduleBackground(schedule, position: position)
+                .frame(height: 11)
+        )
+        .frame(height: 11)
+    }
+    
+    private func scheduleBackground(
+        _ schedule: Schedule,
+        position: SchedulePosition
+    ) -> some View {
+        let color = schedule.type.color.opacity(0.3)
+
+        switch position {
+        case .single:
+            return AnyView(
+                RoundedRectangle(cornerRadius: 2)
+                    .fill(color)
+            )
+
+        case .start:
+            return AnyView(
+                RoundedCorner(radius: 2, corners: [.topLeft, .bottomLeft])
+                    .fill(color)
+            )
+
+        case .middle:
+            return AnyView(
+                Rectangle()
+                    .fill(color)
+            )
+
+        case .end:
+            return AnyView(
+                RoundedCorner(radius: 2, corners: [.topRight, .bottomRight])
+                    .fill(color)
+            )
+        }
+    }
+}
+
+#Preview {
+    MainView()
 }
