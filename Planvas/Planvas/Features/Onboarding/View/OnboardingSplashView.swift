b351d39997d2e75c7f14de6cb8e9e1fc67d820bf
hmj6589 <hmj6589@gmail.com>
2026-02-05 14:26:21 +0900

    💄 Design: 온보딩 스플래시 UI 수정 (#42)

--- Planvas/Planvas/Features/Onboarding/View/OnboardingSplashView.swift
+++ Planvas/Planvas/Features/Onboarding/View/OnboardingSplashView.swift
@@ -19,127 +19,45 @@ struct OnboardingSplashView: View {
                 endPoint: .top)
             .ignoresSafeArea()
             
-            VStack {
-                TabView(selection: $currentPage) {
-                    OnboardingSplash1
-                        .tag(0)
-                    OnboardingSplash2
-                        .tag(1)
-                }
-                .tabViewStyle(.page(indexDisplayMode: .never))
+            VStack(spacing: 0) {
+                Spacer()
                 
-                VStack(spacing: 17) {
-                    HStack(spacing: 8) {
-                        ForEach(0..<2) { index in
-                            Capsule()
-                                .fill(currentPage == index ? .primary20 : .gray888)
-                                .frame(width: currentPage == index ? 32 : 14, height: 8)
-                        }
-                    }
+                Image("OnboardingSplash")
+                    .padding(.bottom, 81)
+                
+                Text("막연한 감 대신,")
+                    .textStyle(.semibold22)
+                    .foregroundStyle(.black1)
+                    .padding(.bottom, 1)
+                
+                HStack(spacing: 0) {
                     
-                    ZStack {
-                        PrimaryButton(title: "목표 설정하러 가기") {
-                            print("목표 설정하러 가기 버튼 클릭")
-                            
-                            // 온보딩 스플래시 두번째 화면 이동 로직
-                            router.push(.onboardingSplashSuccess)
-                        }
-                        
-                        // 비활성화 처리
-                        if currentPage == 0 {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .fill(.primary1)
-                                RoundedRectangle(cornerRadius: 12)
-                                    .fill(.black20)
-
-                                Text("목표 설정하러 가기")
-                                    .textStyle(.semibold20)
-                                    .foregroundStyle(.fff50)
-                            }
-                            .frame(height: 56)
-                            .allowsHitTesting(true)
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    .zIndex(1)
+                    Text("나만의 균형")
+                        .textStyle(.semibold25)
+                        .foregroundStyle(.primary1)
+                    
+                    Text("을 세우러 가볼까요?")
+                        .textStyle(.semibold22)
+                        .foregroundStyle(.black1)
                 }
-                .padding(.bottom, 90)
+                .padding(.bottom, 115)
+                
+                // 목표 설정하러 가기 버튼
+                PrimaryButton(title: "목표 설정하러 가기") {
+                    print("목표 설정하러 가기 버튼 클릭")
+                    
+                    // 온보딩 스플래시 두번째 화면 이동 로직
+                    router.push(.onboardingSplashSuccess)
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 66)
+                .zIndex(1)
             }
-            .navigationBarBackButtonHidden(true)
-            .toolbar(.hidden, for: .navigationBar)
         }
-    }
-
-    private var OnboardingSplash1: some View {
-        VStack(spacing: 0) {
-            Spacer()
-            
-            Text("학기, 방학, 시험 기간 등...")
-                .textStyle(.medium18)
-                .foregroundStyle(.black1)
-                .padding(.bottom, 10)
-            
-            HStack(spacing: 0) {
-                Text("지금의 ")
-                    .textStyle(.bold25)
-                    .foregroundStyle(.black1)
-                
-                Text("시간")
-                    .textStyle(.bold25)
-                    .foregroundStyle(.primary1)
-                
-                Text("이")
-                    .textStyle(.bold25)
-                    .foregroundStyle(.black1)
-            }
-            .padding(.bottom, 1)
-            
-            HStack(spacing: 0) {
-                Text("어떤 모습")
-                    .textStyle(.bold25)
-                    .foregroundStyle(.primary1)
-                
-                Text("으로 기억되고 싶나요?")
-                    .textStyle(.bold25)
-                    .foregroundStyle(.black1)
-            }
-            .padding(.bottom, 55)
-            
-            Image("OnboardingSplash1")
-                .padding(.bottom, 128)
-            
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .navigationBarBackButtonHidden(true)
+        .toolbar(.hidden, for: .navigationBar)
     }
     
-    private var OnboardingSplash2: some View {
-        VStack(spacing: 0) {
-            Spacer()
-            
-            Image("OnboardingSplash2")
-                .padding(.bottom, 66)
-            
-            Text("막연한 감 대신,")
-                .textStyle(.bold22)
-                .foregroundStyle(.black1)
-                .padding(.bottom, 1)
-            
-            HStack(spacing: 0) {
-                
-                Text("나만의 균형")
-                    .textStyle(.bold30)
-                    .foregroundStyle(.primary1)
-                
-                Text("을 세우러 가볼까요?")
-                    .textStyle(.bold22)
-                    .foregroundStyle(.black1)
-            }
-            .padding(.bottom, 100)
-
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
 }
 
 #Preview {
