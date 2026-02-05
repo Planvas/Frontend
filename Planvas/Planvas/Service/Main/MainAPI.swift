d286bebb34b4030680e55fc7b40eea34c7551d16
mzxxzysy <jsyjj21@gmail.com>
2026-02-06 03:33:29 +0900

    ✨ Feat: 메인 페이지 API 연동 세팅 (#36)

--- Planvas/Planvas/Service/Main/MainAPI.swift
+++ Planvas/Planvas/Service/Main/MainAPI.swift
@@ -5,3 +5,34 @@
 //  Created by 정서영 on 2/2/26.
 //
 
+import Foundation
+import Moya
+import Alamofire
+
+// MARK: - 홈 API 연결
+enum MainAPI {
+    case getMainData // 홈 대시보드 조회
+}
+
+extension MainAPI: APITargetType {
+    var path: String {
+        switch self {
+        case .getMainData:
+            return "/api/home"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getMainData:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getMainData:
+            return .requestPlain
+        }
+    }
+}
