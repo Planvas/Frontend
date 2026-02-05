9d3125b6344819afc0ff1318d3cd3fba34320140
minkyo <smk78449@naver.com>
2026-02-05 15:37:31 +0900

    ✨ Feat: 지난 시즌 리포트 페이지 ViewModel 구현(#34)

---
+++ Planvas/Planvas/Features/Report/PastReport/ViewModel/PastReportViewModel.swift
@@ -0,0 +1,47 @@
+import Foundation
+import Combine
+import Moya
+
+class PastReportViewModel:ObservableObject {
+    @Published var reportsByYear: [Int: [PastReportSuccessResponse.Seasons]] = [:]
+    @Published var sortedYears: [Int] = []
+    
+    @Published var errorMessage: String?
+    @Published var isLoading: Bool = false
+
+    private let provider = TokenProvider<PastReportRouter>(isStub: true)
+    private var cancellable = Set<AnyCancellable>()
+    
+    // MARK: - 지난 시즌 리포트 조회
+    func fetchPastReport(year: Int? = nil) {
+        provider.requestPublisher(.getPastReport(year:year))
+            .receive(on: DispatchQueue.main)
+            .handleEvents(
+                receiveSubscription: { [weak self] _ in self?.isLoading = true },
+                receiveCompletion: { [weak self] _ in self?.isLoading = false },
+                receiveCancel: { [weak self] in self?.isLoading = false }
+            )
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                    print("Network error:", error)
+                }
+            }, receiveValue: { [weak self] (response: PastReportResponse) in
+                self?.errorMessage = nil
+                if response.resultType == "SUCCESS", let seasons = response.success?.seasons {
+                    self?.groupReportsByYear(seasons: seasons.compactMap { $0 })
+                } else {
+                    self?.errorMessage = response.error?.reason ?? "알 수 없는 오류 발생"
+                }
+            })
+            .store(in: &cancellable)
+    }
+    
+    // MARK: - 시즌별 리포트를 오름차순으로 저장(연도별로)
+    private func groupReportsByYear(seasons: [PastReportSuccessResponse.Seasons]) {
+        // [연도: [해당 연도 리포트]]
+        let group = Dictionary(grouping: seasons) { $0.year }
+        self.reportsByYear = group
+        self.sortedYears = group.keys.sorted(by: <)
+    }
+}
