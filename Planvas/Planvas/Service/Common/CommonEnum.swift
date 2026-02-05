507708859e66ad9b220c0d460d51c928c021ca07
mzxxzysy <jsyjj21@gmail.com>
2026-02-06 06:25:43 +0900

    ♻️ Refactor: 공통 카테고리 분리 (#36)

---
+++ Planvas/Planvas/Service/Common/CommonEnum.swift
@@ -0,0 +1,12 @@
+//
+//  CommonEnum.swift
+//  Planvas
+//
+//  Created by 정서영 on 2/6/26.
+//
+
+// 성장/휴식 카테고리
+enum TodoCategory: String, Codable {
+    case growth = "GROWTH"
+    case rest = "REST"
+}
