549828d4083eac8edbdbca0aecf2e1a2bef0670e
minkyo <smk78449@naver.com>
2026-02-10 13:01:10 +0900

    🐛 Fix: RootView가 RootRouter의 상태 변화를 감지하도록 수정 (#47)

--- Planvas/Planvas/Core/Navigation/RootView.swift
+++ Planvas/Planvas/Core/Navigation/RootView.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 struct RootView: View {
     @EnvironmentObject var container: DIContainer
+    @ObservedObject var router: RootRouter
+    
+    init(container: DIContainer) {
+        self.router = container.rootRouter
+    }
     
     var body: some View {
         switch container.rootRouter.root {
