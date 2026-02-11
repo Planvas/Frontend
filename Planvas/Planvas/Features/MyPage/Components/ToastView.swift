4bec1fa14cc45149b7a3fddd7cbce3941d126fa7
minkyo <smk78449@naver.com>
2026-02-11 19:10:59 +0900

    💄 Design: 화면에 넘치지 않게 토스트메시지 크기 조정 (#52)

--- Planvas/Planvas/Features/MyPage/Components/ToastView.swift
+++ Planvas/Planvas/Features/MyPage/Components/ToastView.swift
@@ -9,8 +9,10 @@ struct ToastView: View {
             .foregroundColor(.white)
             .padding(.vertical, 12)
             .padding(.horizontal, 20)
+            .multilineTextAlignment(.center)
             .background(Color.black.opacity(0.8))
             .cornerRadius(25)
             .shadow(radius: 10)
+            .frame(maxWidth: 400)
     }
 }
