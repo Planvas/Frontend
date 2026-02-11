ef71401c3c1ab23b11fdb9e508262c8c7b5243a3
mzxxzysy <jsyjj21@gmail.com>
2026-02-11 21:34:31 +0900

    ✨ Feat: 알림 설정 변경 API 연동 (#50)

--- Planvas/Planvas/Features/MyPage/ViewModel/NotificationViewModel.swift
+++ Planvas/Planvas/Features/MyPage/ViewModel/NotificationViewModel.swift
@@ -9,9 +9,10 @@ import Foundation
 import Combine
 import Moya
 
+@Observable
 class NotificationViewModel {
-    @Published var reminder: Bool = false
-    @Published var complete: Bool = true
+    var reminder: Bool = true
+    var complete: Bool = true
     
     private let provider = APIManager.shared.createProvider(for: NotificationsAPI.self)
     
@@ -33,6 +34,32 @@ class NotificationViewModel {
                 }
             case .failure(let error):
                 print("GetNotifications API 오류: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - 알림 설정 변경
+    func fetchReminderSetting() {
+        let dto = NotificationSettingRequestDTO(
+            dDayReminderEnabled: reminder,
+            activityCompleteReminderEnabled: complete
+        )
+        provider.request(.patchNotification(NotificationSettingRequestDTO: dto)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let decodedData = try JSONDecoder().decode(NotificationResponse.self, from: response.data)
+                    DispatchQueue.main.async {
+                        if let success = decodedData.success {
+                            self.reminder = success.dDayReminderEnabled
+                            self.complete = success.activityCompleteReminderEnabled
+                        }
+                    }
+                } catch {
+                    print("Notifications 세팅 디코더 오류: \(error)")
+                }
+            case .failure(let error):
+                print("Notifications 세팅 API 오류: \(error)")
             }
         }
     }
