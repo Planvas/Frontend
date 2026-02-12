9ab6ace7558b60d3d5b901e59d806db712b8f118
hmj6589 <hmj6589@gmail.com>
2026-02-12 16:19:59 +0900

    ✨ Feat: 활동 카드 팁메세지 추가 (#69)

--- Planvas/Planvas/Features/Activity/Components/ActivityCard.swift
+++ Planvas/Planvas/Features/Activity/Components/ActivityCard.swift
@@ -45,7 +45,8 @@ struct ActivityCardView: View {
                             .stroke(.ccc, lineWidth: 0.5))
                         .padding(7)
                 }
-                .padding(.vertical, 13)
+                .padding(.top, 13)
+                .padding(.bottom, item.tip == nil ? 13 : 0)
 
                 VStack(alignment: .leading, spacing: 10) {
                     HStack(alignment: .top, spacing: 0) {
@@ -69,8 +70,12 @@ struct ActivityCardView: View {
                     Text(item.title)
                         .textStyle(.semibold16)
                 }
-                
             }
+            
+            if let tip = item.tip {
+                TipMessageView(tip: tip)
+            }
+            
         }
         .padding(.horizontal, 13)
         .background(.white)
@@ -82,19 +87,102 @@ struct ActivityCardView: View {
         .shadow(color: .ccc, radius: 4, x:0, y: 2)
         .frame(maxWidth: .infinity)
     }
+    
+    private struct TipMessageView: View {
+        let tip: ActivityTip
+        
+        var body: some View {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.subPurple)
+                    .frame(maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.primary1, lineWidth: 0.5)
+                    )
+                
+                HStack(spacing: 0) {
+                    Text(tip.label) // "Tip" or "주의"
+                        .textStyle(.semibold12)
+                        .foregroundStyle(.fff)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                .fill(.primary1)
+                        )
+                    
+                    Text(tip.tag)   // "카페 알바"
+                        .textStyle(.bold12)
+                        .foregroundStyle(.primary1)
+                        .lineLimit(1)
+                        .padding(.leading, 8)
+                    
+                    Text(tip.message)
+                        .textStyle(.medium12)
+                        .foregroundStyle(.primary1)
+                        .lineLimit(1)
+                        .padding(.leading, 2)
+                    
+                    Spacer()
+                }
+                .padding(.vertical, 7)
+                .padding(.horizontal, 12)
+            }
+            .frame(height: 36)
+            .padding(.bottom, 12)
+        }
+    }
 }
 
 #Preview {
-    ActivityCardView(
-        item: ActivityCard(
-            imageURL: nil,
-            badgeText: "일정 가능",
-            badgeColor: .blue1,
-            growth: 20,
-            dday: 5,
-            title: "패스트 캠퍼스 2026 AI 대전환 오픈 세미나"
+    VStack(spacing: 12) {
+        ActivityCardView(
+            item: ActivityCard(
+                imageURL: nil,
+                badgeText: "일정 가능",
+                badgeColor: .blue1,
+                growth: 10,
+                dday: 9,
+                title: "패스트 캠퍼스 2026 AI 대전환 오픈 세미나",
+                tip: nil
+            )
         )
-    )
-    .padding()
-}
 
+        ActivityCardView(
+            item: ActivityCard(
+                imageURL: nil,
+                badgeText: "일정 주의",
+                badgeColor: .yellow1,
+                growth: 20,
+                dday: 15,
+                title: "2026 빅데이터 분석 자격증 온라인 교육생 모집",
+                tip: ActivityTip(
+                    label: "Tip",
+                    tag: "[카페 알바]",
+                    message: "일정이 있어요! 시간을 쪼개서 계획해 보세요",
+                    labelColor: .primary1
+                )
+            )
+        )
+
+        ActivityCardView(
+            item: ActivityCard(
+                imageURL: nil,
+                badgeText: "일정 겹침",
+                badgeColor: .red1,
+                growth: 30,
+                dday: 20,
+                title: "제4회 2026 블레이버스 MVP 개발 해커톤",
+                tip: ActivityTip(
+                    label: "주의",
+                    tag: "[일본 여행]",
+                    message: "일정과 겹쳐요!",
+                    labelColor: .red1
+                )
+            )
+        )
+    }
+    .padding()
+    .background(Color.fff)
+}
