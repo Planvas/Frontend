6bce32633e2780f689071cabf4096a614969e34e
minkyo <smk78449@naver.com>
2026-02-12 14:57:28 +0900

    ✨ Feat: 장바구니 ViewModel 구현 (#70)

--- Planvas/Planvas/Features/Activity/ViewModel/CartViewModel.swift
+++ Planvas/Planvas/Features/Activity/ViewModel/CartViewModel.swift
@@ -1,7 +1,38 @@
-//
-//  CartViewModel.swift
-//  Planvas
-//
-//  Created by 송민교 on 2/11/26.
-//
+import Moya
+import Combine
+import CombineMoya
+import Foundation
+import Observation
 
+@Observable
+class CartViewModel {
+    var cartSuccessData: CartListSuccess?
+    var errorMessage: String?
+    var isLoading: Bool = false
+    var toastMessage: String?
+    var showToast: Bool = false
+    
+    private let provider = APIManager.shared.testProvider(for: ActivityAPI.self)
+    private var cancellable = Set<AnyCancellable>()
+    
+    // MARK: - 장바구니 조회
+    func fetchCartList(for tab: TodoCategory) {
+        self.isLoading = true
+        provider.requestPublisher(.getCartList(tab: tab))
+            .map(CartListResponse.self)
+            .receive(on: DispatchQueue.main)
+            .handleEvents(
+                receiveSubscription: { [weak self] _ in self?.isLoading = true },
+                receiveCompletion: { [weak self] _ in self?.isLoading = false },
+                receiveCancel: { [weak self] in self?.isLoading = false}
+            )
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                }
+            }, receiveValue: { [weak self] response in
+                self?.cartSuccessData = response.success
+            })
+            .store(in: &cancellable)
+    }
+}
