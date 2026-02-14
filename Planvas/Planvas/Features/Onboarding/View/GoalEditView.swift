ec0bc879c3c6df2faced2c8cb8ea915c94f05090
hmj6589 <hmj6589@gmail.com>
2026-02-14 21:23:17 +0900

    ♻️ Refactor: 불필요한 주석 제거 (#104)

--- Planvas/Planvas/Features/Onboarding/View/GoalEditView.swift
+++ Planvas/Planvas/Features/Onboarding/View/GoalEditView.swift
@@ -73,19 +73,17 @@ struct GoalEditView: View {
         .task {
             print("수정 페이지 진입 완료. 현재 설정된 이름: \(vm.goalName), Step: \(vm.ratioStep)")
         }
-        // 2. 시작 날짜 선택 시트
         .sheet(isPresented: $isShowingStartDatePicker) {
             datePickerSheet(title: "시작 날짜 선택", selection: Binding(
                 get: { vm.startDate ?? Date() },
                 set: { vm.startDate = $0 }
             ))
         }
-        // 3. 종료 날짜 선택 시트 (시작 날짜 이후만 선택 가능하도록 제한)
         .sheet(isPresented: $isShowingEndDatePicker) {
             datePickerSheet(title: "종료 날짜 선택", selection: Binding(
                 get: { vm.endDate ?? Date() },
                 set: { vm.endDate = $0 }
-            ), range: (vm.startDate ?? Date())...) // 시작일 이후만 선택 가능
+            ), range: (vm.startDate ?? Date())...)
         }
     }
     
@@ -183,7 +181,7 @@ struct GoalEditView: View {
             HStack(alignment: .center) {
                 // 시작날짜 수정하기 버튼
                 Button{
-                    // TODO: 시작날짜 수정 로직
+                    // 시작날짜 수정 로직
                     isShowingStartDatePicker = true
                     
                 } label: {
@@ -204,7 +202,7 @@ struct GoalEditView: View {
                 
                 // 끝날짜 수정하기 버튼
                 Button{
-                    // TODO: 끝날짜 수정 로직
+                    // 끝날짜 수정 로직
                     isShowingEndDatePicker = true
                     
                 } label: {
@@ -424,6 +422,7 @@ struct GoalEditView: View {
         .cornerRadius(10)
     }
     
+    // MARK: - 날짜 Tuple
     private func dateFromTuple(_ tuple: (year: String, month: String, day: String)?) -> Date? {
         guard let tuple = tuple else { return nil }
         guard
@@ -444,7 +443,7 @@ struct GoalEditView: View {
         return Calendar.current.date(from: comp)
     }
     
-    // 4. 날짜 수정을 위한 공통 시트 뷰
+    // MARK: - 날짜 수정을 위한 시트
     private func datePickerSheet(title: String, selection: Binding<Date>, range: PartialRangeFrom<Date>? = nil) -> some View {
         NavigationStack {
             VStack {
@@ -469,7 +468,7 @@ struct GoalEditView: View {
                 }
             }
         }
-        .presentationDetents([.medium]) // 절반 높이로 표시
+        .presentationDetents([.medium])
     }
 }
 
