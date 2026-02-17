36c2522547e260e10652ac17c90a1beb8e04ef97
minkyo <smk78449@naver.com>
2026-02-17 10:33:09 +0900

    ♻️ Refactor: 리포트 API 파일 내 sampleData 삭제 (#70)

--- Planvas/Planvas/Service/Report/PastReportAPI.swift
+++ Planvas/Planvas/Service/Report/PastReportAPI.swift
@@ -24,34 +24,4 @@ extension PastReportRouter: APITargetType {
             return .requestPlain
         }
     }
-    
-    // TODO: - 서버 구축 시 sampleData 제거
-    var sampleData: Data {
-        let json = """
-            {
-                "resultType":"SUCCESS",
-                "error":null,
-                "success":{
-                    "seasons":[
-                        {
-                            "goalId":12,
-                            "title":"겨울방학 갓생",
-                            "startDate":"2025-12-22",
-                            "endDate":"2026-02-28",
-                            "year":2025
-                        },
-                        {
-                            "goalId":9,
-                            "title":"2학기 기말 대비",
-                            "startDate":"2025-11-20",
-                            "endDate":"2025-12-15",
-                            "year":2025
-                        }
-                    ]
-                }
-            }
-            """
-        
-        return json.data(using: .utf8)!
-    }
 }
