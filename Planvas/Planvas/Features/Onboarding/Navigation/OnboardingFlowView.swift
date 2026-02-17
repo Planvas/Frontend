98ec4d814eb4e2e10ec7e561c4acbaba0688404a
minkyo <smk78449@naver.com>
2026-02-17 11:58:45 +0900

    ♻️ Refactor: 런타임크래시 방지를 위한 viewModel 제공 (#67)

--- Planvas/Planvas/Features/Onboarding/Navigation/OnboardingFlowView.swift
+++ Planvas/Planvas/Features/Onboarding/Navigation/OnboardingFlowView.swift
@@ -70,6 +70,7 @@ struct OnboardingFlowView: View {
         .environment(container.goalVM)
         .environment(container.onboardingVM)
         .environment(container.loginVM)
+        .environment(NavigationRouter<MyPageRoute>())
     }
 }
 
